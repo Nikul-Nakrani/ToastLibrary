02b71ba2e8800b04d6c1e6b633361efce5678922
Nikul-Nakrani <nikul.nakrani@horizoncore.in>
2024-01-10 18:21:17 +0530

    toast length change

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 class ToasterMessage {
     companion object {
         fun s(c: Context, message: String) {
-            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+            Toast.makeText(c, message, Toast.LENGTH_LONG).show()
         }
     }
 }
