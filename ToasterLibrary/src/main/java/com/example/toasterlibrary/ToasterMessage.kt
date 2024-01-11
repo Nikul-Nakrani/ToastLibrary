794f3fc2e26c958ad6b6bf37dc8f8380b6d3df54
Nikul-Nakrani <nikul.nakrani@horizoncore.in>
2024-01-11 15:59:48 +0530

    first commit

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.widget.Toast
 
 class ToasterMessage {
-    companion object {
-        fun s(c: Context, message: String) {
-            Toast.makeText(c, message, Toast.LENGTH_LONG).show()
-        }
+
+    fun s(c: Context, message: String) {
+        Toast.makeText(c, message, Toast.LENGTH_LONG).show()
     }
+
 }
